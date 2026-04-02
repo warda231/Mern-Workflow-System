62198d7782c353f94a493877fba0a9f84fbc522f
Werdah Batool <104501022+warda231@users.noreply.github.com>
2026-04-02 12:47:53 +0500

    install new packages & add auth , role middleware

---
+++ backend/Middleware/role.js
@@ -0,0 +1,15 @@
+exports.roleMiddleware= async (...roles)=>{
+    return (req,res,next)=>{
+        if(!req.user){
+            return res.status(401).json({
+                message: "User not authenticated",
+              });
+        }
+        if(!roles.includes(req.user.role)){
+            return res.status(403).json({
+                message: "Access denied: insufficient permissions",
+              });
+        }
+        next();
+    };
+};
