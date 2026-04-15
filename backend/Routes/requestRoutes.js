19c56ef367c6578697151cdc6a7f2ddb2db8d422
Werdah Batool <104501022+warda231@users.noreply.github.com>
2026-04-15 15:39:38 +0500

    Initialized React frontend

--- backend/Routes/requestRoutes.js
+++ backend/Routes/requestRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const { authMiddleware } = require("../Middleware/auth.js");
 const { roleMiddleware } = require("../Middleware/role.js");
+const {body}=require("express-validator");
+const  validate= require("../Middleware/Validate.js");
 
 const {
     createRequest,
@@ -12,7 +14,21 @@ const {
     rejectRequest,
 }=require("../Controllers/requestController.js");
 
-router.post("/",authMiddleware,createRequest);
+router.post("/",authMiddleware,[
+    body("title")
+    .notEmpty()
+    .withMessage("Title is required")
+    .isLength({min:3})
+    .withMessage("Title must be atleast of 3 characters"),
+
+    body("description")
+    .notEmpty()
+    .withMessage("Description is required")
+    .isLength({min:5})
+    .withMessage("Description must be at least 5 characters"),
+
+
+],validate,createRequest);
 router.get("/me",authMiddleware,getMyRequests);
 router.get("/",authMiddleware,roleMiddleware("admin"),getAllRequests);
 router.put("/approve/:id",authMiddleware,roleMiddleware("manager"),approveRequest);
