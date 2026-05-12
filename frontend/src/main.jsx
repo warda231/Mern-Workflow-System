35275bbc41c23e82e0bc21c01daa6988ac70c8dd
Werdah Batool <104501022+warda231@users.noreply.github.com>
2026-05-12 11:34:43 +0500

    Configured toast notification provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import {Toaster} from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+  <Toaster position="top-right"/>
     <App />
   </BrowserRouter>
 );
